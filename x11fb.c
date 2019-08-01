4e1b792e0f2585710ea2f50c66dd7ddb589f2ec2
Peter Gielda <pgielda@antmicro.com>
2019-08-01 08:50:16 +0200

    Update x11fb.c.

--- x11fb.c
+++ x11fb.c
@@ -15,41 +15,52 @@
 #define WIDTH 640
 #define HEIGHT 480
 
+typedef struct {
+	Window window;
+	XImage *image;
+	int width;
+	int height;
+} _internal_surface;
+
+#define MAX_SURFACES 255
+
 static Display *display = NULL;
-static Window window;
-static XImage *img;
-static int width = 0;
-static int height = 0;
+static _internal_surface surfaces[MAX_SURFACES];
+int surface_count = 0;
 
 // get framebuffer data
-uint32_t *fb_getdata() {
-	return (uint32_t*)img->data;
+uint32_t *fb_getdata(int surface_id) {
+	return (uint32_t*)surfaces[surface_id].image->data;
 }
 
-// redraw fb
-void fb_redraw() {
-	XPutImage(display,window,DefaultGC(display,DefaultScreen(display)),img,0,0,0,0,width,height);
-	XFlush(display);
+// partial redraw
+void fb_redraw_rect(int surface_id, int x, int y, int w, int h) {
+        XPutImage(display,surfaces[surface_id].window,DefaultGC(display,DefaultScreen(display)),surfaces[surface_id].image,x,y,x,y,w,h);
+        XFlush(display);
 }
 
-// create fb window
-void init_fb_window(int w, int h) {
-	static int init = 1;
-	if (init) {
+// redraw whole fb
+void fb_redraw(int surface_id) {
+	fb_redraw_rect(surface_id, 0, 0, surfaces[surface_id].width,surfaces[surface_id].height);
+}
+
+int create_fb_window(int w, int h) {
+	if (!display) {
 		display = XOpenDisplay(NULL);
-		Window root = RootWindow(display,DefaultScreen(display));
-        	window = XCreateSimpleWindow(display,root,50,50,WIDTH,HEIGHT,1,0,0); // TODO: should be centered?
-        	XSelectInput(display,window,/*ExposureMask |*/ ButtonPressMask | ButtonReleaseMask | KeyPressMask | KeyReleaseMask | PointerMotionMask);
-        	XMapWindow(display,window);
-		width = w;
-		height = h;
-		init = 0;
-		img = XCreateImage(display,DefaultVisual(display,DefaultScreen(display)),DefaultDepth(display,DefaultScreen(display)),ZPixmap, 0,(char*)malloc(w*h*4),w,h,32,0);
 	}
+	Window root = RootWindow(display,DefaultScreen(display));
+	surfaces[surface_count].width = w;
+	surfaces[surface_count].height = h;
+	surfaces[surface_count].window = XCreateSimpleWindow(display,root,50,50,surfaces[surface_count].width,surfaces[surface_count].height,1,0,0); // TODO: should be centered?
+        XSelectInput(display,surfaces[surface_count].window, ButtonPressMask | ButtonReleaseMask | KeyPressMask | KeyReleaseMask | PointerMotionMask);
+        XMapWindow(display,surfaces[surface_count].window);
+	surfaces[surface_count].image = XCreateImage(display,DefaultVisual(display,DefaultScreen(display)),DefaultDepth(display,DefaultScreen(display)),ZPixmap, 0,(char*)malloc(w*h*4),w,h,32,0);
+	surface_count++;
+	return (surface_count - 1);
 }
 
 // cleanup
-void free_fb_window() {
+void free_fb_window(int surface_id) {
 	// TODO
 }
 
@@ -65,7 +76,7 @@ unsigned long long msepoch() {
 
 int main(int argc,char **argv)
 {
-        init_fb_window(WIDTH, HEIGHT);
+        int sid = create_fb_window(WIDTH, HEIGHT);
 
         XEvent event;
 	printf("Press ESC or 'q' to exit.\n");
@@ -74,10 +85,10 @@ int main(int argc,char **argv)
         while(1) {
 		unsigned long long initial_ms = msepoch();
 		// generate some noise
-		uint32_t *data = fb_getdata();
+		uint32_t *data = fb_getdata(sid);
 		for (int x = 0; x < WIDTH; x++) for (int y = 0; y < HEIGHT; y++) data[x+y*WIDTH] = rand() % 0xFFFFFF;
-		fb_redraw();
-		if(XCheckWindowEvent(display, window, ButtonPressMask | ButtonReleaseMask | KeyPressMask | KeyReleaseMask | PointerMotionMask, &event)) {
+		fb_redraw(sid);
+		if(XCheckWindowEvent(display, surfaces[sid].window, ButtonPressMask | ButtonReleaseMask | KeyPressMask | KeyReleaseMask | PointerMotionMask, &event)) {
                     if(event.type == KeyPress) {
 		    	if (event.xkey.keycode == 0x9 || event.xkey.keycode == 0x18) break;
 			printf("Key press, code 0x%x\n", event.xkey.keycode);
@@ -87,6 +98,6 @@ int main(int argc,char **argv)
 		int64_t sleep_time = 16 - msepoch() + initial_ms;
 		if (sleep_time > 0) usleep(1000*sleep_time); // wait to 1/60s
         }
-	free_fb_window();
+	free_fb_window(sid);
         return 0;
 }
